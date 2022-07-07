82c2d0b8ae266abc58c41cc2af27e1aa998a4236
LEE KEAT KEAN <keatkean@gmail.com>
2022-07-07 17:00:49 +0800

    add generate numbers func

--- RestAPICaller/Form1.cs
+++ RestAPICaller/Form1.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace RestAPICaller
 {
     public partial class Form1 : Form
@@ -9,7 +11,18 @@ namespace RestAPICaller
 
         private void buttonSequence_Click(object sender, EventArgs e)
         {
+          
+        }
 
+        // generate a private function to generate number 1 to 1400 and store in a list
+        private List<int> GenerateNumberList()
+        {
+            var numberList = new List<int>();
+            for (var i = 1; i <= 1400; i++)
+            {
+                numberList.Add(i);
+            }
+            return numberList;
         }
     }
 }
