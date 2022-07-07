1c29ea64023b1ce16dd67d042bddcd8f637cbf70
lee_keat_kean <lee_keat_kean@nyp.edu.sg>
2022-07-07 17:53:45 +0800

    Added call http call function

--- RestAPICaller/Form1.cs
+++ RestAPICaller/Form1.cs
@@ -1,9 +1,18 @@
 using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Timer = System.Threading.Timer;
 
 namespace RestAPICaller
 {
     public partial class Form1 : Form
     {
+        private readonly List<int> _numberList = new();
+        private HttpClient httpClient = new HttpClient();
+        private const string ApiUrl = "http://localhost:5000/lightup";
         public Form1()
         {
             InitializeComponent();
@@ -11,18 +20,33 @@ namespace RestAPICaller
 
         private void buttonSequence_Click(object sender, EventArgs e)
         {
-          
+            foreach (var number in _numberList)
+            {
+                var model = new Model
+                {
+                    id = number.ToString()
+                };
+
+                var content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8,"application/json"); 
+                httpClient.PostAsync(ApiUrl,content);
+                Thread.Sleep(10);
+            }
+            
         }
 
         // generate a private function to generate number 1 to 1400 and store in a list
-        private List<int> GenerateNumberList()
+        private void GenerateNumberList()
         {
-            var numberList = new List<int>();
             for (var i = 1; i <= 1400; i++)
             {
-                numberList.Add(i);
+                _numberList.Add(i);
             }
-            return numberList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateNumberList();
+            
         }
     }
 }
---
+++ RestAPICaller/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPICaller
+{
+    public class Model
+    {
+        public string id { get; set; }
+
+    }
+}
