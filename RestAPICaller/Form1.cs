e861d5ae48d900a8dcedce3bb2fe083528433a1a
lee_keat_kean <lee_keat_kean@nyp.edu.sg>
2022-07-19 16:28:28 +0800

    Update Form1.cs

--- RestAPICaller/Form1.cs
+++ RestAPICaller/Form1.cs
@@ -1,10 +1,5 @@
-using System.Net;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using Timer = System.Threading.Timer;
 
 namespace RestAPICaller
 {
